4bc11b000fd7eb60ef006cb32ad788288a55719c
wupangzai <1530765818@qq.com>
2021-06-29 15:10:18 +0800

    '目录更新'

--- src/App.vue
+++ src/App.vue
@@ -13,12 +13,6 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/* 导入base样式 */
+  @import "./assets/css/base.css";
 </style>
